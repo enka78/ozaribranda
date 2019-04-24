29f7baf4473e57873a5454096ea10a19156cd2b9
engin <engin.kayali@beyn.com.tr>
2019-04-24 21:08:25 +0300

    upload file bitti

---
+++ backend/api/uploadFile.php
@@ -0,0 +1,53 @@
+<?php
+require 'database.php';
+
+
+$response = array();
+$upload_dir = 'uploads/';
+$server_url = 'http://localhost:4200';
+if($_FILES["uploadFile"])
+{
+	//$avatar_name = explode(".", $_FILES["file"]["name"]);
+	$avatar_name = $_FILES["file"]["name"];
+	$avatar_tmp_name = $_FILES["uploadFile"]["tmp_name"];
+	$error = $_FILES["uploadFile"]["error"];
+	
+    if($error > 0){
+        $response = array(
+            "status" => "error",
+            "error" => true,
+            "message" => "Error uploading the file!"
+        );
+    }else 
+    {
+        $random_name = rand(1000,1000000)."-". $avatar_name;
+        $upload_name = $upload_dir.strtolower($avatar_name);
+        $upload_name = preg_replace('/\s+/', '-', $upload_name);
+		
+    
+        if(move_uploaded_file($avatar_tmp_name , $upload_name)) {
+            $response = array(
+                "status" => "success",
+                "error" => false,
+                "message" => "File uploaded successfully",
+                "url" => $server_url."/".$upload_name
+              );
+        }else
+        {
+            $response = array(
+                "status" => "error",
+                "error" => true,
+                "message" => "Error uploading the file!"
+            );
+        }
+    }
+    
+}else{
+    $response = array(
+        "status" => "error",
+        "error" => true,
+        "message" => "No file was sent!"
+    );
+}
+echo json_encode($response);
+ ?>
