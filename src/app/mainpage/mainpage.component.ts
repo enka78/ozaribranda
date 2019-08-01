531994cd3b98798688683a4fb18738d183ddbe97
Engin Kayali <engin.kayali@beyn.com.tr>
2019-08-01 19:01:05 +0300

    dinamik meta tag yapıldı

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 
 @Component({
@@ -8,8 +9,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MainpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title, private meta: Meta) { }
 
+  keywords = 'Branda, Çadır,Tente, Kamp Çadırı,1100 PVC,450 PVC TENTELİK, Yerli Akrilik ,  Avrupa Akrilik , File, Jüt,   Gazebo Çardak,  Katlanır Tente ,  Örümcek Çadır ,  Oto Brandası ,  Katlanır Masa ,  Ahşap Masa ,  Katlanır Sandalye ,  Ahşap Rejisör ,  Ebatlı Branda , Rulo Branda , Minder, Armut Minder,  Asma Germe ,  Konfeksiyon Torbası ,  Konfeksiyon Torbası ,   Taşıma Torbası , Kapsül, Halka,  Fermuar , Toka,  Off Road Araç Yan Tentesi';
   ngOnInit() {
+    this.titleService.setTitle('özarı branda mehmet balarısı');
+    this.meta.addTag({name: 'description', content: 'Branda,  Çadır ve Malzemeleri'});
+    this.meta.addTag({name: 'keywords', content: this.keywords});
   }
 }
