64ccab1b1be94eea02eb0bc4d82081e7d657d1be
Engin Kayali <engin.kayali@beyn.com.tr>
2019-08-08 14:54:25 +0300

    dinamik meta tag yapıldı

--- src/app/navbarlink/navbarlink.component.ts
+++ src/app/navbarlink/navbarlink.component.ts
@@ -1,4 +1,6 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
+import {Kategori} from '../models/kategori';
+import {ApiService} from '../services/api.service';
 
 @Component({
   selector: 'app-navbarlink',
@@ -7,10 +9,18 @@ import {Component, ElementRef, OnInit} from '@angular/core';
 })
 export class NavbarlinkComponent implements OnInit {
   lightStatus: boolean = true;
-  constructor(private elementRef: ElementRef) { }
+  urunlerLink: string;
+  constructor(private elementRef: ElementRef, private apiService: ApiService) { }
 
   ngOnInit() {
+    this.getKategori();
   }
+  getKategori() {
+    this.apiService.readKategori().subscribe((kategori: Kategori[]) => {
+      this.urunlerLink = kategori[0].kategoriAdi.replace(/\s/gi, '').toLowerCase();
+    });
+  }
+
   lightChange() {
     this.lightStatus = !this.lightStatus;
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.lightStatus ? 'white' : 'black';
